1100b2ca4767e9b41546226db8c4b0da000ed898
Ivan Passoni <ipassoni@gmail.com>
2020-02-29 16:58:35 -0500

    create make_data.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description=''Housing price prediction machine learning project'',
+    description='Housing price prediction machine learning project',
     author='RYSIM',
     license='',
 )
